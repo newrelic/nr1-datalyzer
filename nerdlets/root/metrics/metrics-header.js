19b40a19c29733d5e158bf84473122bd9eebc713
Daniel Golden <dgolden@newrelic.com>
2019-08-27 16:53:13 -0500

    Fixes long attribute name issues and utilty bar styling

--- nerdlets/root/metrics/metrics-header.js
+++ nerdlets/root/metrics/metrics-header.js
@@ -10,8 +10,9 @@ export default class MetricsHeader extends React.Component {
   render() {
 
     return <div className="utility-bar">
-      <AccountPicker {...this.props} className="freedom" />
-      <DataTypePicker {...this.props} dataType="metric" className="freedom"/>
+      <AccountPicker {...this.props} />
+      <DataTypePicker {...this.props} dataType="metric" />
+      <hr />
       <MetricPicker {...this.props} {...this.state}/>
       <FunctionPicker {...this.props} {...this.state}/>
     </div>
