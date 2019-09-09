bf57d88b9a7371aeb25ee90fe1a721859da26da6
DevFreddy <michael.frederickjr@gmail.com>
2019-09-09 11:45:39 -0400

    ESLint/prettier formatting

--- nerdlets/root/account-picker.js
+++ nerdlets/root/account-picker.js
@@ -1,29 +1,33 @@
-import React from "react"
-import { Dropdown, DropdownItem } from "nr1"
+import React from 'react';
+import { Dropdown, DropdownItem } from 'nr1';
 
 export default class AccountPicker extends React.PureComponent {
   render() {
-    let { account, accounts, setAccount } = this.props
-    const {filter} = (this.state || {})
-    
-    if(filter && filter.length > 0) {
-      const re = new RegExp(filter, 'i')
+    let { account, accounts, setAccount } = this.props;
+    const { filter } = this.state || {};
+
+    if (filter && filter.length > 0) {
+      const re = new RegExp(filter, 'i');
       accounts = accounts.filter(a => {
-        return a.name.match(re)
-      })
+        return a.name.match(re);
+      });
     }
 
-    return <Dropdown
-    title={account.name}
-    filterable
-    label="Account"
-    onChangeFilter={event => this.setState({ filter: event.target.value })}
-  >
-      {accounts.map(a => {
-        return <DropdownItem key={a.id} onClick={() => setAccount(a)}>
-          {a.name}
-        </DropdownItem>
-      })}
-    </Dropdown>
+    return (
+      <Dropdown
+        title={account.name}
+        filterable
+        label="Account"
+        onChangeFilter={event => this.setState({ filter: event.target.value })}
+      >
+        {accounts.map(a => {
+          return (
+            <DropdownItem key={a.id} onClick={() => setAccount(a)}>
+              {a.name}
+            </DropdownItem>
+          );
+        })}
+      </Dropdown>
+    );
   }
 }
